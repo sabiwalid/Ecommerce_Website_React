57b1967afecb94d88a4656134742339dc27428c6
walidsabi <walidsabi00@gmail.com>
2021-06-19 10:32:28 +0200

    updating my app with a shope page , collection preview and collection item components

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SHOP_DATA from "./shop.data.js";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
 class ShopPage extends React.Component {
   constructor(props) {
@@ -11,7 +12,14 @@ class ShopPage extends React.Component {
   }
 
   render() {
-    return <div>SHOP PAGE</div>;
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
   }
 }
 
